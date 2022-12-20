dca6e9052cd86b8cad804d83f8729ed8028787a8
Julian Holzhey <jah11550@gmail.com>
2022-12-19 23:05:31 -1000

    Added terrain with sample assets

---
+++ Assets/Scripts/CreateWalls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWalls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        jdnfjkd
+        ddfd firstdv
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
